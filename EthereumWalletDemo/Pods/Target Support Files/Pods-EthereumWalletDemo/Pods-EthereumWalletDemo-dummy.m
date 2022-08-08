1f2b9b1b8ad61a3ac0b8402f1462392aff864db2
Leon Han <leon.han@ef.com>
2022-08-08 18:32:42 +0800

    add pod files

---
+++ EthereumWalletDemo/Pods/Target Support Files/Pods-EthereumWalletDemo/Pods-EthereumWalletDemo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EthereumWalletDemo : NSObject
+@end
+@implementation PodsDummy_Pods_EthereumWalletDemo
+@end
---
+++ EthereumWalletDemo/Pods/Target Support Files/Pods-EthereumWalletDemoTests/Pods-EthereumWalletDemoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_EthereumWalletDemoTests : NSObject
+@end
+@implementation PodsDummy_Pods_EthereumWalletDemoTests
+@end
